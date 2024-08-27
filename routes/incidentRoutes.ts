6d7a8d107e9f9d55bb67ebcbb4ba4a7abbb0c364
Jan-Korst <54290818+Jan-Korst@users.noreply.github.com>
2024-08-27 13:48:41 +0300

    error handling update

--- routes/incidentRoutes.ts
+++ routes/incidentRoutes.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,33 +8,69 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+    res.status(500).send('An unexpected error occurred.');
+};
+
+const checkValidId = (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    if (!id || isNaN(Number(id))) {
+        res.status(400).send('Invalid ID format. ID must be a number.');
+    } else {
+        next();
+    }
+};
+
 app.post('/incidents', (req: Request, res: Response) => {
-    console.log('Creating an incident...');
-    res.status(201).send('Incident created.');
+    try {
+        console.log('Creating an incident...');
+        res.status(201).send('Incident created.');
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get('/incidents', (req: Request, res: Response) => {
-    console.log('Retrieving all incidents...');
-    res.status(200).send('All incidents retrieved.');
+    try {
+        console.log('Retrieving all incidents...');
+        res.status(200).send('All incidents retrieved.');
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.get('/incidents/:id', (req: Request, res: Response) => {
-    const { id } = req.params;
-    console.log(`Retrieving incident with ID: ${id}`);
-    res.status(200).send(`Incident ${id} retrieved.`);
+app.get('/incidents/:id', checkValidId, (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        console.log(`Retrieving incident with ID: ${id}`);
+        res.status(200).send(`Incident ${id} retrieved.`);
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.put('/incidents/:id', (req: Request, res: Response) => {
-    const { id } = req.params;
-    console.log(`Updating incident with ID: ${id}`);
-    res.status(200).send(`Incident ${id} updated.`);
+app.put('/incidents/:id', checkValidId, (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        console.log(`Updating incident with ID: ${id}`);
+        res.status(200).send(`Incident ${id} updated.`);
+    } catch (error) {
+        next(error);
+    }
 });
 
-app.delete('/incidents/:id', (req: Request, res: Response) => {
-    const { id } = req.params;
-    console.log(`Deleting incident with ID: ${id}`);
-    res.status(204).send();
+app.delete('/incidents/:id', checkValidId, (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        console.log(`Deleting incident with ID: ${id}`);
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Incident management server running on port ${port}`);
