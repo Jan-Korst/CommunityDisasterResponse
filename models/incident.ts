46e2c5b753674f03da175b1901a8c089ec69ab87
Jan-Korst <54290818+Jan-Korst@users.noreply.github.com>
2024-11-14 02:21:49 +0200

    Pushed incident.ts

---
+++ models/incident.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+enum Severity {
+    Low = 'Low',
+    Medium = 'Medium',
+    High = 'High',
+}
+
+interface Location {
+    latitude: string;
+    longitude: string;
+}
+
+interface Incident {
+    description: string;
+    location: Location;
+    severity: Severity;
+    userId: string;
+}
+
+const API_KEY: string = process.env.API_KEY || '';
+
+export { Incident, Location, Severity };
